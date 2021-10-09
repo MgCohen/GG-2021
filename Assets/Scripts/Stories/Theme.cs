4f3459a5d580b954f66f78d4a37723ee5eb824cc
matheuscohen <matheuscohen@hotmail.com>
2021-10-09 11:28:54 -0300

    Base

---
+++ Assets/Scripts/Stories/Theme.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Theme")]
+public class Theme : ScriptableObject
+{
+    public Sprite icon;
+}
+
+public class ThemeLevel
+{
+    public Theme theme;
+    public int level;
+}
